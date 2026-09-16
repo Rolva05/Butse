d684a793f2738cb467b5ff5cd322bbbd993988d5
Rolva05 <Kamogelorolva03@gmail.com>
2026-09-16 16:35:28 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -3,11 +3,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
-import { AppProvider } from "./context/AppContext";
-
-// Temporarily enable debug CSS to reveal layout (will revert later)
-// Debug stylesheet lives in the project-level `styles/` folder
-import "../styles/debug.css";
+import { AppProvider } from "./AppContext";
+import "./global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 
